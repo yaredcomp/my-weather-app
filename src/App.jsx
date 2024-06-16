5a4f71f9cdd393387cd7af9a3d8cfd2478d5dbbb
yaredderejeamu <yaredderejeamu@gmail.com>
2024-06-16 13:58:52 +0000

    add tailwind

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import { useState } from "react";
 import reactLogo from "./assets/react.svg";
 import viteLogo from "/vite.svg";
 import "./App.css";
+import Weather from "./Componets/Weather";
 
 function App() {
   const [count, setCount] = useState(0);
@@ -9,7 +10,7 @@ function App() {
   return (
     <>
       <div>
-        <h1>Welcome to React</h1>
+        <Weather />
       </div>
     </>
   );
